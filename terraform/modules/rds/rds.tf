6e5ac8328a566daebddf6ef12d13e81a414586a3
Pramod Badiger <80105491+Pramod858@users.noreply.github.com>
2025-02-18 14:24:23 +0530

    code updated

--- terraform/modules/rds/rds.tf
+++ terraform/modules/rds/rds.tf
@@ -1,38 +1,48 @@
+# Create a DB subnet group for the RDS cluster
 resource "aws_db_subnet_group" "rds_subnet" {
     name       = "${var.environment}-rds-subnet"
-    subnet_ids = [var.private_subnet_3_id,var.private_subnet_4_id]
+    subnet_ids = [var.private_subnet_3_id, var.private_subnet_4_id]
 
     tags = {
         Name = "${var.environment}-rds-subnet"
     }
 }
 
+# RDS Cluster
 resource "aws_rds_cluster" "db_cluster" {
     cluster_identifier      = "${var.environment}-db-cluster"
     engine                  = "aurora-mysql"
-    engine_version	        = "8.0.mysql_aurora.3.08.0"
-    port                    = "3306"
+    engine_version          = "8.0"  # Ensure this is a valid version for aurora-mysql
+    port                    = 3306
     database_name           = var.db_name
     master_username         = var.db_username
     master_password         = var.db_password
-	backtrack_window        = 0
-    skip_final_snapshot     = true
+    backtrack_window        = 0
+    skip_final_snapshot     = true  # Be cautious in production environments
     db_subnet_group_name    = aws_db_subnet_group.rds_subnet.name
     availability_zones      = ["${var.region}a", "${var.region}b"]
     vpc_security_group_ids  = [var.db_security_group_id]
 
-    serverlessv2_scaling_configuration {
-        min_capacity = 2
-        max_capacity = 4
-    }
-
     backup_retention_period = 0
 
     lifecycle {
-        ignore_changes = [serverlessv2_scaling_configuration, backup_retention_period, availability_zones]
+        ignore_changes = [backup_retention_period, availability_zones]
     }
 
     tags = {
         Name = "${var.environment}-db-cluster"
+    }
+}
+
+# RDS Cluster Instance
+resource "aws_rds_cluster_instance" "rds_cluster_instance" {
+    identifier           = "rds_aurora_cluster_instance"
+    cluster_identifier   = aws_rds_cluster.db_cluster.id
+    instance_class       = "db.t3.medium"
+    db_subnet_group_name = aws_db_subnet_group.rds_subnet.name
+    depends_on           = [aws_rds_cluster.db_cluster]
+
+    tags = {
+        Name = "${var.environment}-db-cluster-instance"
     }
 }
