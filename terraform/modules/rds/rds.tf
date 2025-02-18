a8d250a646dbb438972b7b520089c39969b6a37f
Pramod Badiger <80105491+Pramod858@users.noreply.github.com>
2025-02-18 10:42:33 +0530

    Update rds.tf

--- terraform/modules/rds/rds.tf
+++ terraform/modules/rds/rds.tf
@@ -10,26 +10,27 @@ resource "aws_db_subnet_group" "rds_subnet" {
 resource "aws_rds_cluster" "db_cluster" {
     cluster_identifier      = "${var.environment}-db-cluster"
     engine                  = "aurora-mysql"
-    engine_mode             = "serverless"
-    engine_version          = "5.7.mysql_aurora.2.11.4"
+    engine_version          = "5.7.mysql_aurora.2.11.2"
     port                    = "3306"
     database_name           = var.db_name
     master_username         = var.db_username
     master_password         = var.db_password
-    backtrack_window        = 0
+	backtrack_window        = 0
     enable_http_endpoint    = true
     skip_final_snapshot     = true
     db_subnet_group_name    = aws_db_subnet_group.rds_subnet.name
-    availability_zones      = ["${var.region}a","${var.region}b"] 
+    availability_zones      = ["${var.region}a", "${var.region}b"]
     vpc_security_group_ids  = [var.db_security_group_id]
-    scaling_configuration {
-        min_capacity = 2
+
+    serverlessv2_scaling_configuration {
+        min_capacity = 1
+        max_capacity = 2
     }
 
     backup_retention_period = 0
 
     lifecycle {
-        ignore_changes = [scaling_configuration, backup_retention_period, availability_zones]
+        ignore_changes = [serverlessv2_scaling_configuration, backup_retention_period, availability_zones]
     }
 
     tags = {
